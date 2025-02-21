8f904e48bd7112b80cf5287e86f8e738041c8e83
rsalazar <ronal.salazarcasas@gmail.com>
2025-02-20 21:55:28 -0500

    Salidas por consola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hola clase");
+        System.out.println("Estamos aprendiendo comandos git");
+        System.out.println("Repasando comandos");
 
     }
 }
